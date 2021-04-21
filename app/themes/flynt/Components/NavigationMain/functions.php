3ff3bf53bb334e6d11f7df5977b6304bd522931d
Matthew Dixon <matthewdixondev@gmail.com>
2021-04-21 14:05:16 +0100

    feat: flynt 1.4.0

---
+++ app/themes/flynt/Components/NavigationMain/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flynt\Components\NavigationMain;
+
+use Timber;
+use Flynt\Utils\Asset;
+
+add_action('init', function () {
+    register_nav_menus([
+        'navigation_main' => __('Navigation Main', 'flynt')
+    ]);
+});
+
+add_filter('Flynt/addComponentData?name=NavigationMain', function ($data) {
+    $data['menu'] = new Timber\Menu('navigation_main');
+    $data['logo'] = [
+        'src' => get_theme_mod('custom_header_logo') ? get_theme_mod('custom_header_logo') : Asset::requireUrl('Components/NavigationMain/Assets/logo.svg'),
+        'alt' => get_bloginfo('name')
+    ];
+
+    return $data;
+});
