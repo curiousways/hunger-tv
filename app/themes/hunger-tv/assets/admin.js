d73b916f629a779185e2426bb5c64545a8b4aa33
Matthew Dixon <matthewdixondev@gmail.com>
2021-04-27 15:53:41 +0100

    feat: tidy assets

--- app/themes/hunger-tv/assets/admin.js
+++
@@ -1,8 +0,0 @@
-import './scripts/publicPath'
-import './admin.scss'
-
-function importAll (r) {
-  r.keys().forEach(r)
-}
-
-importAll(require.context('../Components/', true, /\/admin\.js$/))
--- app/themes/hunger-tv/assets/main.js
+++ app/themes/hunger-tv/assets/main.js
@@ -1,31 +1,5 @@
-import './scripts/publicPath'
-import 'console-polyfill'
-import 'normalize.css/normalize.css'
-import './main.scss'
-import $ from 'jquery'
-import feather from 'feather-icons'
+// Scripts
+// import "./scripts/script";
 
-import installCE from 'document-register-element/pony'
-
-window.jQuery = $
-
-window.lazySizesConfig = window.lazySizesConfig || {}
-window.lazySizesConfig.preloadAfterLoad = true
-require('lazysizes')
-
-$(document).ready(function () {
-  feather.replace({
-    'stroke-width': 1
-  })
-})
-
-installCE(window, {
-  type: 'force',
-  noBuiltIn: true
-})
-
-function importAll (r) {
-  r.keys().forEach(r)
-}
-
-importAll(require.context('../Components/', true, /\/script\.js$/))
+// Styles
+import "./main.scss";
