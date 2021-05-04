82bec565cfd4ce04afcb658a86e8a3b3ad2309ef
Matthew Dixon <matthewdixondev@gmail.com>
2021-05-04 12:23:02 +0100

    feat: initial editorial cpt

--- app/themes/hunger-tv/setup/post-types.php
+++ app/themes/hunger-tv/setup/post-types.php
@@ -1,3 +1,14 @@
 <?php
 
 add_post_type_support('page', 'excerpt');
+
+register_post_type('editorial', [
+	'labels' => [
+		'name' => __('Editorials'),
+		'singular_name' => __('Editorial')
+	],
+	'public' => true,
+	'publicly_queryable' => true,
+	'menu_icon' => 'dashicons-media-document',
+	'supports' => ['title', 'editor', 'thumbnail', 'slug', 'revisions']
+]);
