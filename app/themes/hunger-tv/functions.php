9a3ee7536a83936745da65b278585950a41aa44e
Matthew Dixon <matthewdixondev@gmail.com>
2021-05-04 12:42:37 +0100

    feat: move legacy code

--- app/themes/hunger-tv/functions.php
+++ app/themes/hunger-tv/functions.php
@@ -2,3 +2,4 @@
 
 include_once 'setup/timber.php';
 include_once 'setup/post-types.php';
+include_once 'setup/legacy.php';
---
+++ app/themes/hunger-tv/setup/legacy.php
@@ -0,0 +1,23 @@
+<?php
+
+// Legacy
+// require __DIR__ . '/../hungertv-custom/hungertv-add-custom-post-types.php';
+
+// Source: hunger-tv-legacy theme
+function hungertv_parse_credits($credits, $wrap_in_aside="true") {
+    $lines = "";
+    if (strpos($credits, '@@@') !== false) {
+      $lines = explode("\n", str_replace("\n\n", "\n", trim($credits)));
+      for ($i = 0; $i < count($lines); $i++) {
+        if (strpos($credits, '@@@') !== false && trim($lines[$i]) != "") {
+          $lines[$i] = '<b>' . str_replace("@@@", '</b> ', $lines[$i]);
+        }
+      }
+      if ($wrap_in_aside){
+        return '<aside class="credits" data-lines="' . count($lines) .'"><span>' . implode('<br/>', $lines) . "</span></aside>";
+      }else{
+        return '<span>' . implode('<br/>', $lines) . "</span>";
+      }
+
+    }
+  }
