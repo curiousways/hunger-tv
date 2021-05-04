942d96d09b21f3c31f16e637d83da2b34a48e0cf
Matthew Dixon <matthewdixondev@gmail.com>
2021-05-04 17:20:10 +0100

    fix: move yoast to bottom

--- app/themes/hunger-tv/setup/legacy.php
+++ app/themes/hunger-tv/setup/legacy.php
@@ -9,19 +9,19 @@ require __DIR__ . '/../hungertv-custom/hungertv-custom-article-acf-location-rule
 
 // Source: hunger-tv-legacy theme
 function hungertv_parse_credits($credits, $wrap_in_aside="true") {
-    $lines = "";
-    if (strpos($credits, '@@@') !== false) {
-      $lines = explode("\n", str_replace("\n\n", "\n", trim($credits)));
-      for ($i = 0; $i < count($lines); $i++) {
-        if (strpos($credits, '@@@') !== false && trim($lines[$i]) != "") {
-          $lines[$i] = '<b>' . str_replace("@@@", '</b> ', $lines[$i]);
-        }
+  $lines = "";
+  if (strpos($credits, '@@@') !== false) {
+    $lines = explode("\n", str_replace("\n\n", "\n", trim($credits)));
+    for ($i = 0; $i < count($lines); $i++) {
+      if (strpos($credits, '@@@') !== false && trim($lines[$i]) != "") {
+        $lines[$i] = '<b>' . str_replace("@@@", '</b> ', $lines[$i]);
       }
-      if ($wrap_in_aside){
-        return '<aside class="credits" data-lines="' . count($lines) .'"><span>' . implode('<br/>', $lines) . "</span></aside>";
-      }else{
-        return '<span>' . implode('<br/>', $lines) . "</span>";
-      }
-
     }
+    if ($wrap_in_aside){
+      return '<aside class="credits" data-lines="' . count($lines) .'"><span>' . implode('<br/>', $lines) . "</span></aside>";
+    }else{
+      return '<span>' . implode('<br/>', $lines) . "</span>";
+    }
+
   }
+}
