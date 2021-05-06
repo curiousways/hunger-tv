33b07b0a5d546ec9c0cde4ef91d13acbfda02170
Matthew Dixon <matthewdixondev@gmail.com>
2021-05-06 11:22:35 +0100

    feat: more specific gallery

--- app/themes/hunger-tv/assets/scripts/carousel.js
+++ app/themes/hunger-tv/assets/scripts/carousel.js
@@ -1,15 +1,15 @@
 import slick from "slick-carousel";
 import "slick-carousel/slick/slick.scss";
 
-jQuery(".carousel__tray").slick({
+jQuery(".carousel--full .carousel__tray").slick({
 	prevArrow: ".carousel__control--prev",
 	nextArrow: ".carousel__control--next",
 	speed: 500,
 	cssEase: "cubic-bezier(0.4, 0, 0.2, 1)",
-	touchThreshold: 10
+	touchThreshold: 10,
 });
 
-jQuery(".carousel__tray").on(
+jQuery(".carousel--full .carousel__tray").on(
 	"init reInit afterChange",
 	function (event, slick, currentSlide, nextSlide) {
 		// currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
