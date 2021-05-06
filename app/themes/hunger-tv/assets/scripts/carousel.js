2ce8c11010ce4f61fcee38bd6a91e5753347a8d8
Matthew Dixon <matthewdixondev@gmail.com>
2021-05-06 11:04:19 +0100

    feat: carousel controls

--- app/themes/hunger-tv/assets/scripts/carousel.js
+++ app/themes/hunger-tv/assets/scripts/carousel.js
@@ -1,10 +1,12 @@
 import slick from "slick-carousel";
 import "slick-carousel/slick/slick.scss";
 
-jQuery(".carousel").slick({
+jQuery(".carousel__tray").slick({
+	prevArrow: ".carousel__control--prev",
+	nextArrow: ".carousel__control--next",
 	speed: 500,
 	cssEase: "cubic-bezier(0.4, 0, 0.2, 1)",
-	touchThreshold: 10,
+	touchThreshold: 10
 });
 
 console.log("%ccarousel running", "color:green;");
