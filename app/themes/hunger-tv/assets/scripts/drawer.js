ca89966871d2d4200f0396fd1a90e2818e28988d
Matthew Dixon <matthewdixondev@gmail.com>
2021-06-18 14:49:26 +0100

    feat: account for open drawers

--- app/themes/hunger-tv/assets/scripts/drawer.js
+++ app/themes/hunger-tv/assets/scripts/drawer.js
@@ -1,17 +1,21 @@
-document
-	.getElementById("drawer--nav__toggle")
-	.addEventListener("click", function () {
-		this.classList.toggle("is-active");
-		document.getElementById("drawer--nav").classList.toggle("open");
-		document.body.classList.toggle("wide");
-	});
+const navToggle = document.getElementById("drawer--nav__toggle");
+const navDrawer = document.getElementById("drawer--nav");
+const searchToggle = document.getElementById("drawer--search__toggle");
+const searchDrawer = document.getElementById("drawer--search");
 
-document
-	.getElementById("drawer--search__toggle")
-	.addEventListener("click", function () {
-		this.classList.toggle("is-active");
-		document.getElementById("drawer--search").classList.toggle("open");
-		document.body.classList.toggle("wide");
-	});
+navToggle.addEventListener("click", function () {
+	this.classList.toggle("is-active");
+	searchDrawer.classList.remove("open");
+	navDrawer.classList.toggle("open");
+	document.body.classList.toggle("wide");
+});
+
+searchToggle.addEventListener("click", function () {
+	this.classList.toggle("is-active");
+	navToggle.classList.remove("is-active");
+	navDrawer.classList.remove("open");
+	searchDrawer.classList.toggle("open");
+	document.body.classList.toggle("wide");
+});
 
 console.log("%cdrawer running", "color:green;");
