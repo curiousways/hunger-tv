efb67afbeceb639d12061dd4f66ae8827a83119b
Matthew Dixon <matthewdixondev@gmail.com>
2021-06-22 18:10:03 +0100

    chore: formatting

--- app/themes/hunger-tv/setup/legacy.php
+++ app/themes/hunger-tv/setup/legacy.php
@@ -10,21 +10,23 @@ require __DIR__ . '/../hungertv-custom/hungertv-custom-article-acf-location-rule
 
 // Source: hunger-tv-legacy theme
 function hungertv_parse_credits($credits, $wrap_in_aside=false) {
-  $lines = "";
-  if (strpos($credits, '@@@') !== false) {
-    $lines = explode("\n", str_replace("\n\n", "\n", trim($credits)));
-    for ($i = 0; $i < count($lines); $i++) {
-      if (strpos($credits, '@@@') !== false && trim($lines[$i]) != "") {
-        $lines[$i] = '<em>' . str_replace("@@@", '</em> ', $lines[$i]);
-      }
-    }
-    if ($wrap_in_aside){
-      return '<aside class="credits" data-lines="' . count($lines) .'"><span>' . implode('<br>', $lines) . "</span></aside>";
-    }else{
-      return '<p class="credits">' . implode('<br>', $lines) . "</p>";
-    }
-
-  }
+	$lines = "";
+	
+	if (strpos($credits, '@@@') !== false) {
+		$lines = explode("\n", str_replace("\n\n", "\n", trim($credits)));
+		
+		for ($i = 0; $i < count($lines); $i++) {
+			if (strpos($credits, '@@@') !== false && trim($lines[$i]) != "") {
+				$lines[$i] = '<em>' . str_replace("@@@", '</em> ', $lines[$i]);
+			}
+		}
+		
+		if ($wrap_in_aside) {
+			return '<aside class="credits" data-lines="' . count($lines) . '"><span>' . implode('<br>', $lines) . "</span></aside>";
+		} else {
+			return '<p class="credits">' . implode('<br>', $lines) . "</p>";
+		}
+	}
 }
 
 // Fix page 404s
