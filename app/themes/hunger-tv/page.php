79cb9bb8e8e9f63f563ac881653dde5f43f4a7b5
Matthew Dixon <matthewdixondev@gmail.com>
2021-06-23 15:47:12 +0100

    feat: add the excerpt to pages

--- app/themes/hunger-tv/page.php
+++ app/themes/hunger-tv/page.php
@@ -25,4 +25,6 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+$context['subtitle'] = get_the_excerpt();
+
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
