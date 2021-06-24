da4f4411877d44b13719a97b275dfe2bd8b8fbbc
Matthew Dixon <matthewdixondev@gmail.com>
2021-06-24 09:50:00 +0100

    feat: trying to render filtered cards with timber

--- app/themes/hunger-tv/functions.php
+++ app/themes/hunger-tv/functions.php
@@ -2,17 +2,23 @@
 
 function filtered_posts($ignore_ids) {
 	// echo $ignore_ids;
+	$context = Timber::context();
 
-	$posts = new WP_Query([
+	$latest_news = Timber::get_posts([
 		'post_type' => ['article', 'editorial'],
 		'posts_per_page' => 6,
 		'post__not_in' => $ignore_ids
 	]);
 
-	while ($posts->have_posts()) {
+	// while ($latest_news->have_posts()) {
+	foreach ($latest_news as $post) {
 		echo '<div class="col md:6 lg:4">';
-		$posts->the_post();
-		include (locate_template('templates/includes/card.php'));
+		// $latest_news->the_post();
+		// include (locate_template('templates/includes/card.php'));
+
+		$timber_post = new Timber\Post();
+		$context['post'] = $timber_post;
+		Timber::render('includes/card.twig', $context);
 		echo '</div>';
 	}
 }
