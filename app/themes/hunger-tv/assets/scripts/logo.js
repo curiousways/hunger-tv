86ad7f217c735a05ce14324564b0f5229438c93c
Matthew Dixon <matthewdixondev@gmail.com>
2021-06-24 12:11:21 +0100

    feat: initial logo animation

--- app/themes/hunger-tv/assets/scripts/logo.js
+++ app/themes/hunger-tv/assets/scripts/logo.js
@@ -1,17 +1,19 @@
 // Source: https://css-tricks.com/books/greatest-css-tricks/scroll-animation/
-window.addEventListener(
-	"scroll",
-	() => {
-		document.body.style.setProperty("--scroll", window.pageYOffset);
-	},
-	false
-);
+// window.addEventListener(
+// 	"scroll",
+// 	() => {
+// 		document.body.style.setProperty("--scroll", window.pageYOffset);
+// 	},
+// 	false
+// );
 
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 0) {
 		document.querySelector(".header__bar").classList.add("visible");
+		document.querySelector(".hero__logo").classList.add("mini");
 	} else {
 		document.querySelector(".header__bar").classList.remove("visible");
+		document.querySelector(".hero__logo").classList.remove("mini");
 	}
 });
 
