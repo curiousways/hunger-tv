ff46469f34134355adf82f334fd2937989c80028
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-01 09:57:04 +0100

    chore: remove static card sections

--- app/themes/hunger-tv/front-page.php
+++ app/themes/hunger-tv/front-page.php
@@ -4,9 +4,4 @@ $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 
-$context['spotlight'] = Timber::get_posts([
-	'post_type' => ['article', 'editorial'],
-	'posts_per_page' => 9
-]);
-
 Timber::render('front-page.twig', $context);
