9cb2f123a70282a3c6059d7786937cdaa5a439d5
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-01 11:06:23 +0100

    feat: initial image crops

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -1,5 +1,8 @@
 <?php
 
 // Image crops
+add_image_size('grid', 1200, 600, true);
+add_image_size('grid--two-thirds', 780, false, true);
+add_image_size('grid--half', 570, false, true);
 add_image_size('slideshow', false, 460, true);
 add_image_size('portrait', 400, 600, true);
