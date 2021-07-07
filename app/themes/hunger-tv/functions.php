2490416a349c7abb5941faddaf2825625aec9f60
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-07 08:47:48 +0100

    feat: filter first post from query

--- app/themes/hunger-tv/functions.php
+++ app/themes/hunger-tv/functions.php
@@ -27,3 +27,4 @@ include_once 'setup/timber.php';
 include_once 'setup/images.php';
 include_once 'setup/post-types.php';
 include_once 'setup/legacy.php';
+include_once 'setup/filter-posts.php';
---
+++ app/themes/hunger-tv/setup/filter-posts.php
@@ -0,0 +1,35 @@
+<?php
+
+// Source: https://codex.wordpress.org/Making_Custom_Queries_using_Offset_and_Pagination
+
+add_action('pre_get_posts', 'myprefix_query_offset', 1 );
+function myprefix_query_offset(&$query) {
+
+    //Before anything else, make sure this is the right query...
+    if ( ! $query->is_main_query() ) {
+        return;
+    }
+
+    //First, define your desired offset...
+    $offset = 1;
+    
+    //Next, determine how many posts per page you want (we'll use WordPress's settings)
+    $ppp = get_option('posts_per_page');
+
+    //Next, detect and handle pagination...
+    if ( $query->is_paged ) {
+
+        //Manually determine page query offset (offset + current page (minus one) x posts per page)
+        $page_offset = $offset + ( ($query->query_vars['paged']-1) * $ppp );
+
+        //Apply adjust page offset
+        $query->set('offset', $page_offset );
+
+    }
+    else {
+
+        //This is the first page. Just use the offset...
+        $query->set('offset',$offset);
+
+    }
+}
