c8a401d971251ec556fc67b004dcce6a522d983c
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-07 11:07:40 +0100

    feat: first post hero

--- app/themes/hunger-tv/archive.php
+++ app/themes/hunger-tv/archive.php
@@ -40,4 +40,16 @@ if ( is_day() ) {
 
 $context['posts'] = new Timber\PostQuery();
 
+$context['first_post'] = Timber::get_posts([
+	'post_type' => get_post_type(),
+	'posts_per_page' => 1,
+	'tax_query' => [
+		[
+			'taxonomy' => get_queried_object()->taxonomy,
+			'field' => 'term_id',
+			'terms' => [get_queried_object()->term_id]
+		]
+	]
+]);
+
 Timber::render( $templates, $context );
