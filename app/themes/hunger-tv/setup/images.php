276854cbab8d72d0740a6dbadb9ac1d0ff598a85
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-13 16:42:24 +0100

    feat: 16:9 images

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -1,8 +1,8 @@
 <?php
 
 // Image crops
-add_image_size('card', 600, 300, true);
-add_image_size('grid', 1200, 600, true);
+add_image_size('card', 400, 225, true); // 16:9
+add_image_size('grid', 1200, 675, true); // 16:9
 add_image_size('grid--two-thirds', 780, false, true);
 add_image_size('grid--half', 465, false, true);
 add_image_size('hero', 1920, 830, true);
