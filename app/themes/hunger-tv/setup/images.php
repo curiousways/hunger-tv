f1b9a0570302ffe2c9293ad79945e70aa6ee094a
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-13 14:10:30 +0100

    feat: medium hero crop

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -6,5 +6,6 @@ add_image_size('grid', 1200, 600, true);
 add_image_size('grid--two-thirds', 780, false, true);
 add_image_size('grid--half', 465, false, true);
 add_image_size('hero', 1920, 830, true);
+add_image_size('hero--medium', 960, 830, true);
 add_image_size('slideshow', false, 460, true);
 add_image_size('portrait', 400, 600, true);
