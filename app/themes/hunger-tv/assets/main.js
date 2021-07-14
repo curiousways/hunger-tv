bf0b7b8f23bbcce900b250378039b101e567eca6
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-14 08:08:34 +0100

    feat: use javascript to get window height

--- app/themes/hunger-tv/assets/main.js
+++ app/themes/hunger-tv/assets/main.js
@@ -8,3 +8,4 @@ import "./main.scss";
 
 document.documentElement.classList.remove("no-js");
 document.documentElement.setAttribute("data-useragent", navigator.userAgent);
+document.body.style.setProperty("--height", window.innerHeight + "px");
