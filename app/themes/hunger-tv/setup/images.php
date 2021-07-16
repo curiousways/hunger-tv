aba1c69dc2936e132801d390939ab84ef0d188f6
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-16 13:22:34 +0100

    feat: remove unnecessary crops

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -1,11 +1,13 @@
 <?php
 
 // Image crops
-add_image_size('card', 400, 225, true); // 16:9
-add_image_size('grid', 1200, 675, true); // 16:9
-add_image_size('hero', 1920, 830, true); // 192:83
-add_image_size('hero--medium', 960, 415, true); // 192:83
-add_image_size('hero--small', 960, 830, true);
+
+// add_image_size('card', 400, 225, true); // 16:9
+// add_image_size('grid', 1200, 675, true); // 16:9
+// add_image_size('hero', 1920, 830, true); // 192:83
+// add_image_size('hero--medium', 960, 415, true); // 192:83
+// add_image_size('hero--small', 960, 830, true);
 add_image_size('portrait', 400, 600, true);
-add_image_size('slideshow', false, 460, true);
-add_image_size('two-thirds', 780, false, true);
+add_image_size('portrait--large', 800, 1200, true);
+// add_image_size('slideshow', false, 460, true);
+// add_image_size('two-thirds', 780, false, true);
