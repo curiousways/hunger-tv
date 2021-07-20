8c81caa8de0ed7413b2dd9f231462834632014df
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-20 15:18:26 +0100

    feat: rename sizes and add large card

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -1,8 +1,9 @@
 <?php
 
 // 16:9
-add_image_size('card', 400, 225, true);
-add_image_size('grid', 1200, 675, true);
+add_image_size('card', 1200, 675, true);
+add_image_size('card--desktop', 384, 216, true);
+add_image_size('card--large', 2400, 1350, true);
 
 // 192:83
 add_image_size('hero', 1920, 830, true);
@@ -12,8 +13,10 @@ add_image_size('hero--small', 960, 415, true);
 add_image_size('portrait', 400, 600, true);
 add_image_size('portrait--large', 800, 1200, true);
 
-// Various
+// Slideshow
 add_image_size('slideshow', false, 460);
 add_image_size('slideshow--large', false, 920);
-add_image_size('two-thirds', 780, false);
-add_image_size('two-thirds--large', 1560, false);
+
+// Grid
+add_image_size('grid', 780, false);
+add_image_size('grid--large', 1560, false);
