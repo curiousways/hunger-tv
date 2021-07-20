63d845c1ab1b47c688f703acd89a53169f63426a
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-20 13:30:41 +0100

    feat: uncomment and reorganise crops

--- app/themes/hunger-tv/setup/images.php
+++ app/themes/hunger-tv/setup/images.php
@@ -1,13 +1,18 @@
 <?php
 
-// Image crops
+// 16:9
+add_image_size('card', 400, 225, true);
+add_image_size('grid', 1200, 675, true);
 
-// add_image_size('card', 400, 225, true); // 16:9
-// add_image_size('grid', 1200, 675, true); // 16:9
-// add_image_size('hero', 1920, 830, true); // 192:83
-// add_image_size('hero--medium', 960, 415, true); // 192:83
-// add_image_size('hero--small', 960, 830, true);
+// 192:83
+add_image_size('hero', 1920, 830, true);
+add_image_size('hero--medium', 960, 415, true);
+add_image_size('hero--small', 960, 830, true);
+
+// 2:3
 add_image_size('portrait', 400, 600, true);
 add_image_size('portrait--large', 800, 1200, true);
-// add_image_size('slideshow', false, 460, true);
-// add_image_size('two-thirds', 780, false, true);
+
+// Various
+add_image_size('slideshow', false, 460, true);
+add_image_size('two-thirds', 780, false, true);
