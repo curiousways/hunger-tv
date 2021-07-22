6e1465a04db3b1f2dbbf0a6cf2e254fc68b91248
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-22 13:05:50 +0100

    fix: logo flash

--- app/themes/hunger-tv/assets/scripts/logo.js
+++ app/themes/hunger-tv/assets/scripts/logo.js
@@ -10,9 +10,36 @@ window.addEventListener(
 	false
 );
 
-if (window.scrollY == 0 && sessionStorage.getItem("meal") == null) {
+function setCookie(name, value, days) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(";");
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == " ") c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	document.cookie =
+		name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+if (window.scrollY == 0 && getCookie("meal") == null) {
 	container.classList.add("animate");
-	sessionStorage.setItem("meal", "devoured");
+	// sessionStorage.setItem("meal", "devoured");
+	setCookie("meal", "devoured", 1);
 
 	window.addEventListener("scroll", () => {
 		logo.classList.add("animating");
