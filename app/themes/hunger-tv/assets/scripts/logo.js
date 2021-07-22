bcdb6bf457d5db897bcda31df3f119712d5951c0
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-22 13:11:21 +0100

    feat: move cookie functions to file

--- app/themes/hunger-tv/assets/scripts/logo.js
+++ app/themes/hunger-tv/assets/scripts/logo.js
@@ -1,3 +1,4 @@
+import { setCookie, getCookie } from "./functions";
 const container = document.querySelector(".header__bar");
 const logo = document.querySelector(".hero__logo");
 
@@ -9,32 +10,6 @@ window.addEventListener(
 	},
 	false
 );
-
-function setCookie(name, value, days) {
-	var expires = "";
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-		expires = "; expires=" + date.toUTCString();
-	}
-	document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(";");
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == " ") c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	document.cookie =
-		name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-}
 
 if (window.scrollY == 0 && getCookie("meal") == null) {
 	container.classList.add("animate");
