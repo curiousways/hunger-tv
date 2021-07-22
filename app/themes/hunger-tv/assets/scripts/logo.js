eb782789ec5c18ad42211ec779081aadcc342ce6
Matthew Dixon <matthewdixondev@gmail.com>
2021-07-22 12:43:49 +0100

    chore: refactor script

--- app/themes/hunger-tv/assets/scripts/logo.js
+++ app/themes/hunger-tv/assets/scripts/logo.js
@@ -1,6 +1,5 @@
 const container = document.querySelector(".header__bar");
 const logo = document.querySelector(".hero__logo");
-const svg = logo.querySelector("svg");
 
 // Source: https://css-tricks.com/books/greatest-css-tricks/scroll-animation/
 window.addEventListener(
@@ -11,28 +10,18 @@ window.addEventListener(
 	false
 );
 
-if (window.scrollY === 0) {
+if (window.scrollY == 0 && sessionStorage.getItem("meal") == null) {
 	container.classList.add("animate");
+	sessionStorage.setItem("meal", "devoured");
 
 	window.addEventListener("scroll", () => {
 		logo.classList.add("animating");
 
-		if (svg.getBoundingClientRect().top <= 24) {
+		if (logo.querySelector("svg").getBoundingClientRect().top <= 24) {
 			logo.classList.add("fixed");
 			container.classList.add("visible");
 		}
 	});
-
-	if (sessionStorage.getItem("meal") == null) {
-		// if (logo) logo.classList.add("animate");
-		// sessionStorage.setItem("meal", "devoured");
-		// setTimeout(() => {
-		// 	if (logo) logo.classList.add("mini");
-		// }, 1500);
-	} else if (logo) {
-		container.classList.add("visible");
-		logo.classList.add("fixed");
-	}
 } else {
 	container.classList.add("visible");
 	if (logo) logo.classList.add("fixed");
