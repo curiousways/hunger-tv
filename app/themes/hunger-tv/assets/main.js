51eff2a376a9a4476d593f569c6cada297119382
Matthew Dixon <matthewdixondev@gmail.com>
2021-09-30 15:59:30 +0100

    fix: query height if logo

--- app/themes/hunger-tv/assets/main.js
+++ app/themes/hunger-tv/assets/main.js
@@ -1,11 +1,11 @@
+document.documentElement.classList.remove("no-js");
+document.documentElement.setAttribute("data-useragent", navigator.userAgent);
+
+// Styles
+import "./main.scss";
+
 // Scripts
 import "./scripts/carousel";
 import "./scripts/drawer";
 import "./scripts/hero";
 import "./scripts/logo";
-
-// Styles
-import "./main.scss";
-
-document.documentElement.classList.remove("no-js");
-document.documentElement.setAttribute("data-useragent", navigator.userAgent);
