46836a5aff1a760b23913815e9ce3209fac824a5
Matthew Dixon <matthewdixondev@gmail.com>
2022-02-02 11:36:09 +0000

    fix: captions conditional

--- app/themes/hunger-tv/assets/scripts/captions.js
+++ app/themes/hunger-tv/assets/scripts/captions.js
@@ -1,20 +1,23 @@
 const captions = document.getElementsByClassName("caption");
-const captionLineHeight = getComputedStyle(captions[0]).lineHeight; // Get .caption line height
-const captionLineHeightNum = parseInt(captionLineHeight, 10); // Convert to number for calculations
-const captionHeight = captionLineHeightNum * 2;
 
-captions.forEach((caption) => {
-	if (caption.offsetHeight > captionHeight) {
-		const button = document.createElement("button");
-		button.innerHTML = "Show more";
+if (captions.length) {
+	const captionLineHeight = getComputedStyle(captions[0]).lineHeight; // Get .caption line height
+	const captionLineHeightNum = parseInt(captionLineHeight, 10); // Convert to number for calculations
+	const captionHeight = captionLineHeightNum * 2;
 
-		caption.style.height = captionHeight + "px";
-		caption.style.overflow = "hidden";
-		caption.parentNode.appendChild(button);
+	captions.forEach((caption) => {
+		if (caption.offsetHeight > captionHeight) {
+			const button = document.createElement("button");
+			button.innerHTML = "Show more";
 
-		button.addEventListener("click", () => {
-			caption.style.height = "auto";
-			button.remove();
-		});
-	}
-});
+			caption.style.height = captionHeight + "px";
+			caption.style.overflow = "hidden";
+			caption.parentNode.appendChild(button);
+
+			button.addEventListener("click", () => {
+				caption.style.height = "auto";
+				button.remove();
+			});
+		}
+	});
+}
