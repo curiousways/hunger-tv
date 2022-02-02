514858ffc7dc0cc2637503b421d51b596c1150cf
Matthew Dixon <matthewdixondev@gmail.com>
2022-02-02 11:07:48 +0000

    feat: line height variable

--- app/themes/hunger-tv/assets/scripts/captions.js
+++ app/themes/hunger-tv/assets/scripts/captions.js
@@ -1,11 +1,12 @@
 const captions = document.getElementsByClassName("caption");
+const lineHeight = 18;
 
 captions.forEach((caption) => {
-	if (caption.offsetHeight > 36) {
+	if (caption.offsetHeight > lineHeight * 2) {
 		const button = document.createElement("button");
 		button.innerHTML = "Show more";
 
-		caption.style.height = "36px";
+		caption.style.height = lineHeight * 2 + "px";
 		caption.style.overflow = "hidden";
 		caption.parentNode.appendChild(button);
 
