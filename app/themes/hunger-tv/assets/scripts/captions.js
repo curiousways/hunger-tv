524dc623d30030567b1511c6d2fe900defd1b38b
Matthew Dixon <matthewdixondev@gmail.com>
2022-02-02 10:59:44 +0000

    feat: append show more button

--- app/themes/hunger-tv/assets/scripts/captions.js
+++ app/themes/hunger-tv/assets/scripts/captions.js
@@ -2,6 +2,15 @@ const captions = document.getElementsByClassName("caption");
 
 captions.forEach((caption) => {
 	if (caption.offsetHeight > 36) {
+		const button = document.createElement("button");
+		button.innerHTML = "Show more";
+
 		caption.style.height = "36px";
+		caption.style.overflow = "hidden";
+		caption.parentNode.appendChild(button);
+
+		button.addEventListener("click", () => {
+			caption.style.height = "auto";
+		});
 	}
 });
