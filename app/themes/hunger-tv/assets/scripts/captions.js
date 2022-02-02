c2b05e5fa90d67bfe0493712cbc6d485a9c66060
Matthew Dixon <matthewdixondev@gmail.com>
2022-02-02 11:31:13 +0000

    feat: get caption line height dynamically

--- app/themes/hunger-tv/assets/scripts/captions.js
+++ app/themes/hunger-tv/assets/scripts/captions.js
@@ -1,12 +1,14 @@
 const captions = document.getElementsByClassName("caption");
-const lineHeight = 18;
+const captionLineHeight = getComputedStyle(captions[0]).lineHeight; // Get .caption line height
+const captionLineHeightNum = parseInt(captionLineHeight, 10); // Convert to number for calculations
+const captionHeight = captionLineHeightNum * 2;
 
 captions.forEach((caption) => {
-	if (caption.offsetHeight > lineHeight * 2) {
+	if (caption.offsetHeight > captionHeight) {
 		const button = document.createElement("button");
 		button.innerHTML = "Show more";
 
-		caption.style.height = lineHeight * 2 + "px";
+		caption.style.height = captionHeight + "px";
 		caption.style.overflow = "hidden";
 		caption.parentNode.appendChild(button);
 
